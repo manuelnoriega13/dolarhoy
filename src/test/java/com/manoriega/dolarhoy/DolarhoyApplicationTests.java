7783a8c1f760641a39a1bea2be397df0288a5f0d
manuel <manuelnoriega13@gmail.com>
2018-09-26 20:33:46 -0300

    3

--- src/test/java/com/manoriega/dolarhoy/DolarhoyApplicationTests.java
+++ src/test/java/com/manoriega/dolarhoy/DolarhoyApplicationTests.java
@@ -1,38 +1,48 @@
 package com.manoriega.dolarhoy;
 
+import com.manoriega.dolarhoy.model.Dolar;
+import com.manoriega.dolarhoy.model.Euro;
+import com.manoriega.dolarhoy.repository.DolarRepo;
+import com.manoriega.dolarhoy.service.DolarService;
+import com.manoriega.dolarhoy.service.EuroService;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Date;
+
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DolarhoyApplicationTests {
 
+    @Autowired
+    private DolarService dolarService;
+
+    @Autowired
+    private EuroService euroService;
+
+    @Autowired
+    private DolarRepo dolarRepo;
+
     @Test
     public void contextLoads() {
         try {
             Document doc = Jsoup.connect("http://www.dolarhoy.com/cotizacion-dolar").get();
-            Elements element = doc.getElementsByClass("col-md-6 venta");
+            Elements element = doc.getElementsByClass("update");
             Element r2 = element.get(0);
-            r2.getAllElements();
-            Elements r3 = r2.getAllElements();
-            String dolarPrice = r3.get(0).getElementsByTag("span").text().toString();
-            String[] d = dolarPrice.split(" ");
-            String[] d3 = d[1].split(",");
-            String foo = d3[0] + "." + d3[1];
-            Double dolar = Double.parseDouble(foo);
-            System.out.println(dolar);
-//            pull-righ
         } catch (Exception e) {
             System.out.println(e.getMessage());
-
         }
-
     }
 
 }
