96834450822e4bca39cfccfb44274d47642d0149
manuel <manuelnoriega13@gmail.com>
2018-09-27 07:51:47 -0300

    war fo;e

--- src/main/java/com/manoriega/dolarhoy/DolarhoyApplication.java
+++ src/main/java/com/manoriega/dolarhoy/DolarhoyApplication.java
@@ -2,13 +2,19 @@ package com.manoriega.dolarhoy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class DolarhoyApplication {
+public class DolarhoyApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return super.configure(builder);
+    }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		SpringApplication.run(DolarhoyApplication.class, args);
 	}
 }
